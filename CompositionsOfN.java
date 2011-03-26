b6cedb4edaa22fdf8e7b0cf022d36ec7c8646f53
Magnus Hedman <none@unregistered>
2011-03-26 14:25:19 +0100

    yada

--- CompositionsOfN.java
+++ CompositionsOfN.java
@@ -8,12 +8,9 @@
 import java.util.Vector;
 
 public class CompositionsOfN {
-	private Integer n;
-	private Vector<Integer> p;
 	private boolean empty = false;
-	public CompositionsOfN(int n_) {
-		n = new Integer(n_);
-		p = new Vector<Integer>();
+	private Vector<Integer> p = new Vector<Integer>();
+	public CompositionsOfN(int n) {
 		p.add(n);
 	}
 	public boolean hasNext() { return !empty; }
@@ -21,12 +18,15 @@ public class CompositionsOfN {
 		Vector<Integer> r = (Vector<Integer>)p.clone();
 		int i, rsum = 0;
 		// find the rightmost element x of p such that 1 < x
-		for (i = p.size() - 1; 0 <= i && p.get(i) <= 1; i--) { rsum += p.get(i); }
-		if (i == -1) { empty = true; }
+		for (i = p.size() - 1; 0 <= i && p.get(i) <= 1; i--) {
+			rsum += p.get(i);
+		}
+		if (i == -1) {
+			empty = true;
+		}
 		else {
-			int x = p.get(i);
 			p.setSize(i + 1);
-			p.set(i, x - 1);
+			p.set(i, p.get(i) - 1);
 			p.add(rsum + 1);
 		}
 		return r;
